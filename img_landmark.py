9abf5a2a2d04aa606d42ecfa52c113ed0bf0eb4c
Yagyesh Bobde <yagyeshsrbobde@gmail.com>
2022-02-10 10:06:07 +0530

    completed part-1

--- img_landmark.py
+++ img_landmark.py
@@ -2,12 +2,12 @@ import cv2
 import mediapipe as mp
 import time
 
-img = cv2.imread("actress.jpg",cv2.IMREAD_COLOR)
+img = cv2.imread("actors.jpeg",cv2.IMREAD_COLOR)
 
 mpDraw = mp.solutions.drawing_utils
 mpFaceMesh = mp.solutions.face_mesh 
 faceMesh = mpFaceMesh.FaceMesh(max_num_faces=3)
-drawSpec = mpDraw.DrawingSpec(thickness=2, circle_radius=3)
+drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=1)
  
 
 imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
